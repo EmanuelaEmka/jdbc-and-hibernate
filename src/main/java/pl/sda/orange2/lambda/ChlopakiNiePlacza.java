5503e8459782c6ea935f3a855e9acb956ba9dc0c
emetrak <emanuela.metrak@gmail.com>
2023-03-04 13:40:13 +0100

    [add] - lambda exercises extension

---
+++ src/main/java/pl/sda/orange2/lambda/ChlopakiNiePlacza.java
@@ -0,0 +1,14 @@
+package pl.sda.orange2.lambda;
+
+public class ChlopakiNiePlacza implements BoysDontCry {
+
+
+    @Override
+    public void silnoreki() {
+        System.out.println("Jestem silnoręki!");
+    }
+
+    public void sing() {
+        System.out.println("Rusz się...");
+    }
+}
