7c768f9b7e85ef8e187375e71643acced1951713
emetrak <emanuela.metrak@gmail.com>
2023-03-04 14:27:00 +0100

    [change] - lambda examples

--- src/main/java/pl/sda/orange2/lambda/BoysDontCry.java
+++ src/main/java/pl/sda/orange2/lambda/BoysDontCry.java
@@ -1,5 +1,7 @@
 package pl.sda.orange2.lambda;
 
+//it's functional interface because has one abstract method
+
 public interface BoysDontCry {
     void silnoreki();
 }
--- src/main/java/pl/sda/orange2/lambda/LambdaExercises.java
+++ src/main/java/pl/sda/orange2/lambda/LambdaExercises.java
@@ -4,5 +4,33 @@ public class LambdaExercises {
 
     public static void main(String[] args) {
 
+        BoysDontCry boy = new ChlopakiNiePlacza();
+        ChlopakiNiePlacza boy2 = new ChlopakiNiePlacza();
+        boy.silnoreki();
+        boy2.sing();
+
+
+        BoysDontCry anonymousClassReference = new BoysDontCry() {
+            @Override
+            public void silnoreki() {
+                System.out.println("Anonimowy silnoreki:)");
+            }
+        };
+
+        anonymousClassReference.silnoreki();
+
+        // () parenthesis - arguments list
+        // -> arguments, body separator
+        // {} parenthesis - method body
+        //lambda jest metodą anoniową
+
+        BoysDontCry firstLambda = () -> {};
+        BoysDontCry secondLambda = () -> System.out.println("Lambda:)");
+        secondLambda.silnoreki();
+        BoysDontCry thirdLambda = () -> {
+            System.out.println("first");
+            System.out.println("second");
+        };
     }
+
 }
